06da3c9b6f3b4cd0aa301ea342cd8b40e99ebbe4
Gerald <36296159+gabaal@users.noreply.github.com>
2026-04-18 12:13:46 +0000

    UI Bugfix: Enforce correct tab parameter inheritance inside Macro Compass router to prevent orphaned top-level view mounting

--- js/views/macro-analytics.js
+++ js/views/macro-analytics.js
@@ -1,4 +1,5 @@
 async function renderMacroSync(tabs = null) {
+    if (!tabs) tabs = macroHubTabs;
     const tabHTML = tabs ? renderHubTabs('macro', tabs) : '';
     appEl.innerHTML = skeleton(2);
     const [data, sectors, corrData] = await Promise.all([fetchAPI('/macro'), fetchAPI('/sectors'), fetchAPI('/correlation-matrix')]);
